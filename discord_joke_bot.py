cc6b37780b47bca9fc62b13c530c3af5aa724fba
AlanConstantino <constantinoalan98@gmail.com>
2020-02-01 18:52:09 -0800

    Initial commit

---
+++ discord_joke_bot.py
@@ -0,0 +1,27 @@
+import discord
+import joke_api
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$joke'):
+        joke = joke_api.get_joke()
+        print(joke)
+
+        if joke == False:
+            await message.channel.send("Couldn't get joke from API. Try again later.")
+        else:
+            await message.channel.send(joke['setup'] + '\n' + joke['punchline'])
+
+client.run('your token here.')
